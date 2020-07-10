90aba56b1cdc7edd08551dd224dfd581511ac703
WANG Zijian <ziijan0906@foxmail.com>
2020-07-10 09:18:02 +0800

    Enrich setuptools

--- setup.py
+++ setup.py
@@ -6,17 +6,26 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="pyexecutor",
     version="0.0.2",
-    author="Herman",
+    author="WANG ZIJIAN",
     author_email="zijian0906@gmail.com",
-    description="Command executor for Python",
+    description=
+    """
+        A light-weight command executor to run commands and return stdout and stderr.
+    """,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/actini/pyexecutor",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
     ],
-    python_requires='>=3.6',
 )
