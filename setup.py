a027c58857563f9a3fe2915f4f0c577656296ce2
WANG Zijian <ziijan0906@foxmail.com>
2020-07-10 08:59:19 +0800

    Upgrade to version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyexecutor",
-    version="0.0.1",
+    version="0.0.2",
     author="Herman",
     author_email="zijian0906@gmail.com",
     description="Command executor for Python",
